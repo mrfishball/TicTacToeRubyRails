50d5f40d76ac01d47afbb20a27daa509264cd5f1
mrfishball <mrfishball@gmail.com>
2018-11-16 09:36:20 -0500

    Add CORS origins for development, staging and production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'stage-ttt-web-client.herokuapp.com'
+    origins 'live-ttt-web-client.herokuapp', 'stage-ttt-web-client.herokuapp', 'localhost:8080'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
