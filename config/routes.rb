c080362f2c5ac7b55118fc5b0260d0a588b6a27d
mrfishball <mrfishball@gmail.com>
2018-11-16 09:37:24 -0500

    Add /api namespace for all endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  scope module: 'api' do
+  namespace :api, :defaults => { :format => :json }do
     namespace :v1 do
-      resources :game
+      resources :game, only: [ :index ]
+      match 'make_a_move', to: 'make_a_move#make_a_move', via: [:post]
     end
   end
 end
