7b90998a9d7b596421b53297d8a67d7a9528feea
mrfishball <mrfishball@gmail.com>
2019-01-04 15:58:01 -0500

    Attemp to fix CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'live-ttt-web-client.herokuapp', 'stage-ttt-web-client.herokuapp', 'localhost:8080'
+      origins 'https://live-ttt-web-client.herokuapp.com'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
